9e81fb8661e9d7782449ec19f3eee5a9670385ca
05947709392 <samara.goncalves@tjce.jus.br>
2022-01-05 14:34:16 -0300

    Alterando Bundler

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_12_30_164840) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "fruits", force: :cascade do |t|
     t.string "name"
     t.integer "carbohydrates"
